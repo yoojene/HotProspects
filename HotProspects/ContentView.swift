82d82035abd0d7690db8759fa1b4b8e27673a355
Eugene Cross <eugene.cross@gmail.com>
2023-09-18 18:00:35 +0100

    Day 79, TabView intro

--- HotProspects/ContentView.swift
+++ HotProspects/ContentView.swift
@@ -7,43 +7,26 @@
 
 import SwiftUI
 
-
-@MainActor class User: ObservableObject {
-    @Published var name = "Steph Curry"
-}
-
-
-
-struct EditView: View {
-    @EnvironmentObject var user: User
-    
-    var body: some View {
-        TextField("Name", text: $user.name)
-    }
-}
-
-struct DisplayView: View {
-    @EnvironmentObject var user: User // Environment objects use the type as the key and the instance of the type as the value (like a Dict has can hold different types
-    
-    var body: some View {
-        Text(user.name)
-    }
-}
-
-
-
 struct ContentView: View {
     
-    @StateObject var user = User()
-    
+    @State private var selectedTab = "One"
     
     var body: some View {
-        VStack {
-            EditView()
-            DisplayView()
+        TabView(selection: $selectedTab) {
+            Text("Tab 1")
+                .onTapGesture {
+                    selectedTab = "Two"
+                }
+                .tabItem {
+                    Label("One", systemImage: "star")
+                }
+                .tag("One")
+            Text("Tab 2")
+                .tabItem {
+                    Label("Two", systemImage: "circle")
+                }
+                .tag("Two")
         }
-        .environmentObject(user)
-        .padding()
     }
 }
 
