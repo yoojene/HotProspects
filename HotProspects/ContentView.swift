b00efbbb39609d5b4aa1320cdabcc12e711fa70a
Eugene Cross <eugene.cross@gmail.com>
2023-09-19 10:54:31 +0100

    Day 80, Result type

--- HotProspects/ContentView.swift
+++ HotProspects/ContentView.swift
@@ -18,18 +18,42 @@ struct ContentView: View {
             }
     }
     
-    // Works but inflexible
-    // Cannot stash work away whilst other tasks happen
-    // read result elsewhere or cancel the process
+    // a named Task
+    //
+    // can it pass around
+    // return from somewhere else
+    // cancel it  - fetchTask.cancel
+    // let fetchTask: Task<String, Error>
     func fetchReadings() async {
-        do {
+        
+        let fetchTask = Task { () -> String in
             let url = URL(string: "https://hws.dev/readings.json")!
             let (data, _) = try await URLSession.shared.data(from: url)
             let readings = try JSONDecoder().decode([Double].self, from: data)
-            output = "Found \(readings.count) readings"
-        } catch {
-            print("error")
+            return "Found \(readings.count) readings"
         }
+        
+        // result is of type Result<String, Error>
+        // can be passed about no longer async
+        // either value or the error
+        let result = await fetchTask.result
+        
+        // .get() returns the value, but you need to handle the error case
+        do {
+            output = try result.get()
+        } catch {
+            print("Error here")
+        }
+        
+        // alternatively switch
+        
+        switch result {
+        case .success(let str):
+            output = str
+        case .failure(let error):
+            output = "Download error: \(error.localizedDescription)"
+        }
+        
         
     }
 }
