0813cc706284c103da1c55d179f3fe4e05b38a3d
Eugene Cross <eugene.cross@gmail.com>
2023-09-19 11:02:54 +0100

    Day 80, control SwiftUI pixel blending on Image with .interpolation()

--- HotProspects/ContentView.swift
+++ HotProspects/ContentView.swift
@@ -9,52 +9,15 @@ import SwiftUI
 
 
 struct ContentView: View {
-    @State private var output = ""
     
     var body: some View {
-        Text(output)
-            .task {
-                await fetchReadings()
-            }
-    }
-    
-    // a named Task
-    //
-    // can it pass around
-    // return from somewhere else
-    // cancel it  - fetchTask.cancel
-    // let fetchTask: Task<String, Error>
-    func fetchReadings() async {
-        
-        let fetchTask = Task { () -> String in
-            let url = URL(string: "https://hws.dev/readings.json")!
-            let (data, _) = try await URLSession.shared.data(from: url)
-            let readings = try JSONDecoder().decode([Double].self, from: data)
-            return "Found \(readings.count) readings"
-        }
-        
-        // result is of type Result<String, Error>
-        // can be passed about no longer async
-        // either value or the error
-        let result = await fetchTask.result
-        
-        // .get() returns the value, but you need to handle the error case
-        do {
-            output = try result.get()
-        } catch {
-            print("Error here")
-        }
-        
-        // alternatively switch
-        
-        switch result {
-        case .success(let str):
-            output = str
-        case .failure(let error):
-            output = "Download error: \(error.localizedDescription)"
-        }
-        
-        
+        Image("example")
+            .interpolation(.none) // turn off iOS pixel blending.  Stop auto blurring
+            .resizable()
+            .scaledToFit()
+            .frame(maxHeight: .infinity)
+            .background(.black)
+            .ignoresSafeArea()
     }
 }
 
