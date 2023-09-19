95c479bda8c19bb4e13d78abe94c9188264c9c71
Eugene Cross <eugene.cross@gmail.com>
2023-09-19 13:57:14 +0100

    Day 84: saving and loading data with user defaults > encapsulation (private (set) fileprivate)

--- HotProspects/Prospects.swift
+++ HotProspects/Prospects.swift
@@ -15,14 +15,37 @@ class Prospect: Identifiable, Codable {
 }
 
 @MainActor class Prospects: ObservableObject {
-    @Published var people: [Prospect]
+    @Published private(set) var people: [Prospect]
+    let saveKey = "SavedData"
     
     init() {
+        if let data = UserDefaults.standard.data(forKey: saveKey) {
+            if let decoded = try? JSONDecoder().decode([Prospect].self, from: data) {
+                people = decoded
+                return
+            }
+        }
+        
+        // nothing saved
         people = []
+        
+    }
+    
+    private func save() {
+        if let encoded = try? JSONEncoder().encode(people) {
+            UserDefaults.standard.set(encoded, forKey: saveKey)
+        }
+            
+    }
+    
+    func add(_ prospect: Prospect) {
+        people.append(prospect)
+        save()
     }
     
     func toggle(_ prospect: Prospect) {
         objectWillChange.send() // call it before the change!!
         prospect.isContacted.toggle()
+        save()
     }
 }
