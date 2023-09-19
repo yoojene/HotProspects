d8c4c059937746d387b522568692ef3dcd38625b
Eugene Cross <eugene.cross@gmail.com>
2023-09-19 11:10:23 +0100

    Day 81, .contextMenu modifier

--- HotProspects/ContentView.swift
+++ HotProspects/ContentView.swift
@@ -10,14 +10,32 @@ import SwiftUI
 
 struct ContentView: View {
     
+    @State private var backgroundColor = Color.red
+    
     var body: some View {
-        Image("example")
-            .interpolation(.none) // turn off iOS pixel blending.  Stop auto blurring
-            .resizable()
-            .scaledToFit()
-            .frame(maxHeight: .infinity)
-            .background(.black)
-            .ignoresSafeArea()
+        VStack {
+            Text("Hello world")
+                .padding()
+                .background(backgroundColor)
+            
+            Text("Change Color")
+                .padding()
+                .contextMenu { // opened when pressing and holding on an item
+                    Button() {
+                        backgroundColor = .red
+                    } label: {
+                        Label("Red", systemImage: "checkmark.circle.fill")
+                            .foregroundColor(.red) // will be ignored
+                    }
+                    Button("Green") {
+                        backgroundColor = .green
+                    }
+                    Button("Blue") {
+                        backgroundColor = .blue
+                    }
+                }
+        }
+       
     }
 }
 
