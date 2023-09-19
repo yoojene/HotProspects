b2811ae5737af327e36c843f710a1ce4b088a084
Eugene Cross <eugene.cross@gmail.com>
2023-09-19 11:52:00 +0100

    Day 82, building out the tab bar

--- HotProspects/ContentView.swift
+++ HotProspects/ContentView.swift
@@ -10,7 +10,27 @@ import SwiftUI
 struct ContentView: View {
         
     var body: some View {
-        Text("Hello world")
+        TabView {
+            ProspectsView(filter: .none)
+                .tabItem {
+                    Label("Everyone", systemImage: "person.3")
+                }
+            
+            ProspectsView(filter: .contacted)
+                .tabItem {
+                    Label("Contacted", systemImage: "checkmark.circle")
+                }
+            
+            ProspectsView(filter: .uncontacted)
+                .tabItem {
+                    Label("Uncontacted", systemImage: "questionmark.diamond")
+                }
+            
+            MeView()
+                .tabItem {
+                    Label("Me", systemImage: "person.crop.square")
+                }
+        }
     }
 }
 
--- HotProspects/ProspectsView.swift
+++ HotProspects/ProspectsView.swift
@@ -8,13 +8,39 @@
 import SwiftUI
 
 struct ProspectsView: View {
+    
+    enum FilterType {
+        case none, contacted, uncontacted
+        
+    }
+    
+    let filter: FilterType
+    
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+                .navigationTitle(title)
+        }
+     
+        
+    }
+    
+    var title: String {
+        switch filter {
+        case .none:
+            return "Everyone"
+        case .contacted:
+            return "Contacted people"
+        case .uncontacted:
+            return "Uncontacted people"
+        }
+        
     }
 }
 
 struct ProspectsView_Previews: PreviewProvider {
     static var previews: some View {
-        ProspectsView()
+        ProspectsView(filter: .none)
     }
 }
