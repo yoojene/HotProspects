77a0550cbeded367e75b17f26888e5726cb23ec7
Eugene Cross <eugene.cross@gmail.com>
2023-09-19 11:40:31 +0100

    Day 81 reset prior to building project

--- HotProspects/ContentView.swift
+++ HotProspects/ContentView.swift
@@ -10,17 +10,8 @@ import SwiftUI
 
 struct ContentView: View {
         
-    let possibleNumbers = Array(1...60)
-    
     var body: some View {
-        Text(results)
-    }
-    
-    var results: String {
-        let selected = possibleNumbers.random(7).sorted()
-        let strings = selected.map(String.init)
-        
-        return strings.joined(separator: ", ")
+        Text("Hello world")
     }
 }
 
